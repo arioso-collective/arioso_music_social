34e55ebfb56e803e92b148d28e0cc2cc88dc8de4
Sebastian Zeidler <xkx6@txstate.edu>
2025-02-13 12:07:07 -0600

    SCRUM-54 AddUser prevents duplicate users
    Database is currently configured so users cannot sign up for an account with an email that's already in use.

--- Data/Arioso_DB.py
+++ Data/Arioso_DB.py
@@ -1,17 +1,17 @@
-import sqlite3
+import sqlite3, uuid
 
 conn = sqlite3.connect('arioso.db')
 cursor = conn.cursor()
 
 def addUser(userid, username, email):
-    cursor.execute("INSERT INTO users (id, username, email) VALUES (?, ?, ?)", (userid, username, email))
+    cursor.execute("""INSERT OR IGNORE INTO users (id, username, email) VALUES (?, ?, ?)""", (userid, username, email))
     return
 
 def deleteUser(userid):
     cursor.execute("DELETE FROM users WHERE id = ?", (userid,))
     return
 
-deleteUser(1234)
+addUser(1234, 'testuser', 'testuser@arioso.com')
 
 #cursor.execute("INSERT INTO users (id, username, email) VALUES (3398, 'aria', 'aria@arioso.com')")
 conn.commit()
