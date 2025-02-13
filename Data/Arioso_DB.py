356faeb98fa3c7af1bab6823719ab18faa4dc22c
Sebastian Zeidler <xkx6@txstate.edu>
2025-02-13 09:39:58 -0600

    SCRUM-54 Added addUser and deleteUser functions to DB file
    AddUser functioo successfully adds new user to database with desired parameters. DeleteUser successfully removes user from database files.

--- Data/Arioso_DB.py
+++ Data/Arioso_DB.py
@@ -3,6 +3,16 @@ import sqlite3
 conn = sqlite3.connect('arioso.db')
 cursor = conn.cursor()
 
-cursor.execute("INSERT INTO users (id, username, email) VALUES (3398, 'aria', 'aria@arioso.com')")
+def addUser(userid, username, email):
+    cursor.execute("INSERT INTO users (id, username, email) VALUES (?, ?, ?)", (userid, username, email))
+    return
+
+def deleteUser(userid):
+    cursor.execute("DELETE FROM users WHERE id = ?", (userid,))
+    return
+
+deleteUser(1234)
+
+#cursor.execute("INSERT INTO users (id, username, email) VALUES (3398, 'aria', 'aria@arioso.com')")
 conn.commit()
 conn.close()
