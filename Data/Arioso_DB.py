879922d03246ee02e35c67c51de268a0669857a2
Sebastian Zeidler <xkx6@txstate.edu>
2025-02-17 22:13:20 -0600

    SCRUM-15 Added encryptPassword Function
    Added encryptPassword function that uses hash values to encrypt the user's password before storing it in the database.

--- Data/Arioso_DB.py
+++ Data/Arioso_DB.py
@@ -18,6 +18,21 @@ def deleteUser(email):
     cursor.execute("DELETE FROM users WHERE id = ?", (email,))
     return
 
+def addPassword(userid):
+    userpassword = input()
+    encryptedPassword = encryptPassword(userpassword)
+    cursor.execute("INSERT INTO passwords (password, userid) VALUES (?, ?)", (encryptedPassword, userid))
+    return
+
+def encryptPassword(password):
+    sha256_hash = hashlib.sha256()
+    sha256_hash.update(password.encode('utf-8'))
+    encryptedPassword = sha256_hash.hexdigest()
+
+    return encryptedPassword
+
+addPassword(3398)
+
 #cursor.execute("INSERT INTO users (id, username, email) VALUES (3398, 'aria', 'aria@arioso.com')")
 conn.commit()
 conn.close()
