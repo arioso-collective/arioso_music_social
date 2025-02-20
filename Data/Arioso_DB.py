d020fd0e4cd41b493b8ba6c0d8ca675fb523d1f9
Sebastian Zeidler <xkx6@txstate.edu>
2025-02-20 13:07:21 -0600

    SCRUM-16 Split Task in Two
    SCRUM-16 was split into this work and "SCRUM-58" to implement the checkPassword function, as this task took much longer than expected to get functioning. This branch was dedicated to reformatting the code to better fit the changes made to the database.

--- Data/Arioso_DB.py
+++ Data/Arioso_DB.py
@@ -5,56 +5,62 @@ cursor = conn.cursor()
 
 def generateUserID():
     new_userid = uuid.uuid4()
-    int_userid = new_userid.int
-    userid = int_userid >> 64
+    userid = str(new_userid)
     return userid
 
 def addUser(username, email):
     userid = generateUserID()
-    cursor.execute("INSERT OR IGNORE INTO users (id, username, email) VALUES (?, ?, ?)", (userid, username, email))
+    cursor.execute("INSERT OR IGNORE INTO users_new (id, username, email) VALUES (?, ?, ?)", (userid, username, email))
     return
 
 def deleteUser(email):
-    cursor.execute("DELETE FROM users WHERE id = ?", (email,))
+    cursor.execute("DELETE FROM users_new WHERE email = ?", (email,))
     return
 
 def generateSalt(userid):
-    salt = os.urandom(16)
-    cursor.execute ("INSERT OR IGNORE INTO users (id, salt) VALUES (?, ?)", (userid, salt))
+    cursor.execute("SELECT salt FROM passwords WHERE userid = ?", (userid,))
+    usersalt = cursor.fetchone()
+    print(usersalt)
+
+    if usersalt is None:
+        salt = os.urandom(16)
+        print(salt)
+        cursor.execute ("INSERT INTO passwords (userid, password, salt) VALUES (?, ?, ?)", (userid, "", salt))
+    else:
+        salt = usersalt[0]
+        print(salt)
 
     return salt
 
-def encryptPassword(password, userid):
-    salt = generateSalt(userid)
+def encryptPassword(password, salt):
     pbkdf2_hash = hashlib.pbkdf2_hmac('sha256', password.encode(), salt, 100000)
     encryptedPassword = pbkdf2_hash.hex()
 
     return encryptedPassword
 
-def addPassword(userid):
+def addPassword(userid, password):
     cursor.execute("SELECT password from passwords WHERE userid = ?", (userid,))
     userpassword = cursor.fetchone()
 
     if userpassword is None:
-        userpassword = input()
-        encryptedPassword = encryptPassword(userpassword, userid)
-        cursor.execute ("INSERT INTO passwords (password, userid) VALUES (?, ?)", (encryptedPassword, userid))
+        salt = generateSalt(userid)
+        encryptedPassword = encryptPassword(password, salt)
+        cursor.execute ("INSERT OR REPLACE INTO passwords (userid, password, salt) VALUES (?, ?, ?)", (userid, encryptedPassword, salt))
     
     return
 
-def checkPasswordMatch(password, userid):
-   cursor.execute("SELECT password from passwords WHERE userid = ?", (userid,))
-   userpassword = cursor.fetchone()
+#def checkPasswordMatch(password, userid):
+   #cursor.execute("SELECT password from passwords WHERE userid = ?", (userid,))
+   #userpassword = cursor.fetchone()
 
-   encryptedPassword = encryptPassword(password, userid)
+   #encryptedPassword = encryptPassword(password, userid)
 
-   if userpassword == encryptedPassword:
-       return True
-   else:
-       return False
+   #if userpassword == encryptedPassword:
+       #return True
+   #else:
+       #return False
 
-#addPassword(3398)
-#result = checkPasswordMatch("password", 3398)
+#result = checkPasswordMatch("password", 1234)
 #print(result)
 
 conn.commit()
