c0b39788915e85c29594b2ae360c37a6b131628a
Cameron Salazar <mfq16@txstate.edu>
2025-02-20 11:49:58 -0600

    SCRUM-24 updated account_settings_form
    Added code to check if username and email is valid

--- intro_to_flask/account_settings_form.py
+++ intro_to_flask/account_settings_form.py
@@ -5,7 +5,8 @@ sys.dont_write_bytecode = True
 # pip install email_validator
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField, FileField, SubmitField
-from wtforms.validators import DataRequired, Email, Optional, Length, EqualTo
+from wtforms.validators import DataRequired, Email, Optional, Length, EqualTo, ValidationError
+from app.models import User
 
 class AccountSettingsForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
@@ -20,3 +21,13 @@ class AccountSettingsForm(FlaskForm):
     music_preferences = StringField('Favorite Genres', validators=[Optional(), Length(max=100)])
     #profile_picture = FileField('Profile Picture', validators=[FileAllowed(['jpg', 'jpeg', 'png'], 'Images only!')])
     submit = SubmitField('Save Changes')
+
+    def validate_username(self, username):
+        existing_user = User.query.filter_by(username=username.data).first()
+        if existing_user:
+            raise ValidationError('This username is already taken. Please choose another.')
+
+    def validate_email(self, email):
+        existing_email = User.query.filter_by(email=email.data).first()
+        if existing_email:
+            raise ValidationError('This email is already in use. Please login or use a different email.')
