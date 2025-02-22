d4aa9087c912d72838ae170da7d08afca138859a
Sebastian Zeidler <xkx6@txstate.edu>
2025-02-21 20:29:08 -0600

    SCRUM-18 Unit Tests Complete
    All unit tests working for the current database functions.

--- Data/DatabaseTesting.py
+++ Data/DatabaseTesting.py
@@ -7,6 +7,7 @@ class DatabaseTesting(unittest.TestCase):
         self.conn = sqlite3.connect("arioso.db")
         self.cursor = self.conn.cursor()
 
+        #create tables for testing if they don't already exist in the database to prevent errors
         self.cursor.execute("CREATE TABLE IF NOT EXISTS users_new (id TEXT PRIMARY KEY, username TEXT NOT NULL, email TEXT NOT NULL UNIQUE)")
         self.cursor.execute("CREATE TABLE IF NOT EXISTS passwords (userid TEXT PRIMARY KEY, password TEXT, salt BLOB)")
         self.conn.commit()
@@ -25,7 +26,7 @@ class DatabaseTesting(unittest.TestCase):
         email = "aria@arioso.com"
         addUser(self.cursor, username, email)
 
-        
+        #test that both the username and email were added to the database and that they equal the given values
         self.cursor.execute("SELECT username, email FROM users_new WHERE email = ?", (email,))
         result = self.cursor.fetchone()
         self.assertIsNotNone(result)
@@ -37,6 +38,7 @@ class DatabaseTesting(unittest.TestCase):
         testuserid = "000-000-000"
         addPassword(self.cursor,testuserid, testpassword)
 
+        #test that password was added to passwords table
         self.cursor.execute("SELECT password from passwords WHERE userid = ?", (testuserid,))
         result = self.cursor.fetchone()
         self.assertIsNotNone(result)
@@ -45,6 +47,7 @@ class DatabaseTesting(unittest.TestCase):
         testuserid = "000-000-000"
         generateSalt(self.cursor, testuserid)
 
+        #test that salt was added to passwords table
         self.cursor.execute("SELECT salt from passwords WHERE userid =?", (testuserid,))
         result = self.cursor.fetchone()
         self.assertIsNotNone(result)
@@ -54,20 +57,29 @@ class DatabaseTesting(unittest.TestCase):
         email = "aria@arioso.com"
         deleteUser(self.cursor,email)
 
+        #test that user no longer exists in database
         self.cursor.execute("SELECT id FROM users_new WHERE email = ?", (email,))
         result = self.cursor.fetchone()
         self.assertIsNone(result)
     
-    #def test_encryptPassword(self):
-        #password = "password"
-        #salt = os.urandom(16)
+    def test_encryptPassword(self):
+        password = "password"
+        salt = os.urandom(16)
 
-        #result = encryptPassword(password, salt)
-        #self.assertIsNone(result)
-        #self.assertNotEqual(result, password)
+        #test that stored password is encrypted by comparing it to the plain text password
+        result = encryptPassword(password, salt)
+        self.assertIsNotNone(result)
+        self.assertNotEqual(result, password)
 
     
-    #def test_checkPasswordMatch():
+    def test_checkPasswordMatch(self):
+        password = "password"
+        userid = "000-000-000"
+
+        #test that checkPasswordMatch returns a boolean value
+        result = checkPasswordMatch(self.cursor, password, userid)
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, bool)
 
     
 if __name__ == '__main__':
