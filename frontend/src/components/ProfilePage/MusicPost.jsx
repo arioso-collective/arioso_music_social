1ffc265ee591c091e963520459fd80175b0c0ed0
Aaron Reed <qkj11@txstate.edu>
2025-03-12 11:17:21 -0500

    SCRUM-84 added Playable Track to posts

--- frontend/src/components/ProfilePage/MusicPost.jsx
+++ frontend/src/components/ProfilePage/MusicPost.jsx
@@ -5,7 +5,17 @@ const MusicPost = ({ text, song }) => {
   return (
     <div className={styles.postContainer}>
       <p>{text}</p>
-      <div className={styles.songEmbed}>🎶 {song}</div>
+      <div className={styles.songEmbed}>🎶 {song}
+        <div className={styles.audioPlayer}>
+          <audio
+            src="https://prosearch.tribeofnoise.com/artists/show/84070/43981"
+            preload="metadata"
+            className={styles.audioElement}
+            controls
+            controlsList="noplaybackrate"
+          />
+        </div>
+      </div>
     </div>
   );
 };
