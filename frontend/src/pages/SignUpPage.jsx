7b7ae635e2db49e9a2c4684eeb47c6380d93e34e
Cameron Salazar <mfq16@txstate.edu>
2025-03-20 15:51:44 -0500

    SCRUM-110 Implementation of SignUpPage react and css files
    Implemented functionality of SignUpPage react and css files

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,10 +1,16 @@
 import SignUpForm from "../components/SignUpPage/SignUpForm.jsx";
-import "../styles/global.css"; // Import global styles
+import logo from "../assets/Arioso_Logo-03.png"
+import "../pages/SignUpPage.css"; // Import global styles
 
 const SignUpPage = () => {
   return (
     <div className="signup-page">
-      <SignUpForm />
+      <div className="left-section">
+        <img src={logo} alt="Arioso Logo" className="logo" />
+      </div>
+      <div className="right-section">
+        <SignUpForm />
+      </div>
     </div>
   );
 };
