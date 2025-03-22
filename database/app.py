6e68bdc7d88f7a7e035a76fa407dfe5a059c94e6
Sebastian Zeidler <xkx6@txstate.edu>
2025-03-22 11:38:12 -0500

    SCRUM-50 Reformatting and CR(UD) Operations Added
    Decided to move away from the previous layout as it is too complicated for the short time period we have to complete the work. Instead moved all CRUD operations within the single Flask backend app and created routes for creating, finding all users, and finding one user.

--- database/app.py
+++ database/app.py
@@ -1,6 +1,12 @@
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
+from flask import Flask, request, jsonify, render_template, url_for, redirect
+from bson.objectid import ObjectId
+from flask_cors import CORS
 from urllib.parse import quote_plus
+
+app = Flask(__name__)
+cors = CORS(app)
 
 username = "xkx6"
 password = "@ri0s0"
@@ -9,12 +15,52 @@ encoded_password = quote_plus(password)
 
 MONGO_CONNECTION_STRING = f"mongodb+srv://{encoded_username}:{encoded_password}@arioso.8hh3i.mongodb.net/?retryWrites=true&w=majority&appName=Arioso"
 
-try:
-    mongo_client = MongoClient(MONGO_CONNECTION_STRING, serverSelectionTimeoutMS=5000)
-    mongo_client.server_info()  # Check if the server is reachable
-    print("Connected to MongoDB Server...")
+#try:
+mongo_client = MongoClient(MONGO_CONNECTION_STRING, serverSelectionTimeoutMS=5000)
+#mongo_client.server_info()  # Check if the server is reachable
+#print("Connected to MongoDB Server...")
 
-    database = mongo_client['Arioso']
+db = mongo_client['Arioso']
+users_collection = db['users']
+posts_collection = db['posts']
+comments_collection = db['comments']
 
-except ServerSelectionTimeoutError as e:
-    print("Could not connect to the MongoDB server...", e)
+#except ServerSelectionTimeoutError as e:
+    #print("Could not connect to the MongoDB server...", e)
+
+@app.route('/signup', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    if 'name' not in data or 'email' not in data or 'username' not in data or 'password' not in data:
+        return jsonify({"error": "All fields are required"}), 400
+    
+    user = {
+        "name": data['name'],
+        "email": data['email'],
+        "username": data['username'],
+        "password": data['password']
+    }
+    result = users_collection.insert_one(user)
+    user['_id'] = str(result.inserted_id)
+    return redirect(url_for('home'))
+
+@app.route('/read', methods=['GET'])
+def get_users():
+    users = list(users_collection.find())
+    for user in users:
+        user['_id'] = str(user['_id'])
+    return jsonify(users), 200
+
+@app.route('/profile/<username>', methods=['GET'])
+def get_user(username):
+    user = users_collection.find_one({'_id': ObjectId(username)})
+    if user:
+        user['_id'] = str(user['_id'])
+        return jsonify(user), 200
+    return jsonify({"error": "User not found"}), 404
+
+
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
