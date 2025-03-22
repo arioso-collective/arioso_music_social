56efc42885177d904006fe023913969cfde11a96
Sebastian Zeidler <xkx6@txstate.edu>
2025-03-22 12:35:04 -0500

    SCRUM-17 Added hash_password and check_password functionality
    Added two methods that can be called to both hash a user password and check the user password by encrypting it and checking that.

--- database/password_util.py
+++ database/password_util.py
@@ -1,6 +1,10 @@
 import bcrypt
 
 def hash_password(plain_text_password):
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(plain_text_password.encode('utf-8'), salt)
+    return hashed_password
 
-def compare_password(plain_text_password):
+def compare_password(plain_text_password, hashed_password):
+    return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_password)
 
