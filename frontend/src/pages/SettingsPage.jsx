28fe555b84ef76aa9ec23bcf4c0b8341c598e8f6
Cameron Salazar <mfq16@txstate.edu>
2025-04-10 11:19:25 -0500

    SCRUM-143 Created/implemented SettingsPage
    Created and implemented SettingsPage react and css files

---
+++ frontend/src/pages/SettingsPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./SettingsPage.css";
+
+const SettingsPage = () => {
+  return (
+    <div className="settings-page">
+      <h2>Settings</h2>
+      <p>This is where you’ll be able to update your profile, preferences, and more!</p>
+    </div>
+  );
+};
+
+export default SettingsPage;
