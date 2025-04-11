b54c621eedd4d221138b20751f986dddf83736f5
Sebastian Zeidler <xkx6@txstate.edu>
2025-04-11 12:02:50 -0500

    SCRUM-163 Create User Tests Complete
    Added a final all-encompassing test case for creating user that ensures the user was created successfully, while also confirming that the password was handled securely.

--- database/tests_xkx6.py
+++ database/tests_xkx6.py
@@ -7,6 +7,7 @@ from datetime import datetime
 #import HtmlTestRunner
 
 class Tests_XKX6(unittest.TestCase):
+    # Set up Mock MongoDB Database for testing when running each test
     def setUp(self):
         app.config['TESTING'] = True
         self.client = app.test_client()
@@ -18,34 +19,34 @@ class Tests_XKX6(unittest.TestCase):
         import app as app_module
         app_module.users_collection = self.mock_users
 
-    #Test that password is successfully hashed, and that it does not equal the plain_text_password after hashing
+    # Test that password is successfully hashed, and that it does not equal the plain_text_password after hashing
     def test_password_hashing(self):
         plain_text_password = "testpassword123!@#$"
         hashed_password = hash_password(plain_text_password)
         self.assertIsInstance(hashed_password, bytes)
         self.assertNotEqual(plain_text_password.encode('utf-8'), hashed_password)
     
-    #Test that the same password hashed twice returns a unique hash
+    # Test that the same password hashed twice returns a unique hash
     def test_unique_password_hashing(self):
         plain_text_password = "password"
         hashed_password_1 = hash_password(plain_text_password)
         hashed_password_2 = hash_password(plain_text_password)
         self.assertNotEqual(hashed_password_1, hashed_password_2)
 
-    #Test compare password when passwords match
+    # Test compare password when passwords match
     def test_password_comparison_match(self):
         plain_text_password = "!@#$passwordtest"
         hashed_password = hash_password(plain_text_password)
         self.assertTrue(compare_password(plain_text_password, hashed_password))
 
-    #Test compare password when passwords do not match 
+    # Test compare password when passwords do not match 
     def test_password_comparison_non_match(self):
         plain_text_password = "ariosoisthebest"
         incorrect_password = "ariosoistheWORST"
         hashed_password = hash_password(plain_text_password)
         self.assertFalse(compare_password(incorrect_password, hashed_password))
 
-    #Test create user to ensure it requires all fields
+    # Test create user to ensure it requires all fields
     def test_create_user_with_missing_fields(self):
         response = self.client.post('/api/create_user', json={
             "name": "Aria",
@@ -56,7 +57,7 @@ class Tests_XKX6(unittest.TestCase):
         self.assertEqual(response.status_code, 400)
         self.assertIn("All fields are required", response.get_data(as_text=True))
 
-    #Test create user does not allow duplicate usernames
+    # Test create user does not allow duplicate usernames
     def test_create_user_with_duplicate_username(self):
         self.mock_users.insert_one({
             'name': 'Dr. Lehr',
@@ -74,7 +75,7 @@ class Tests_XKX6(unittest.TestCase):
         self.assertEqual(response.status_code, 400)
         self.assertIn('Username already exists', response.get_data(as_text=True))
     
-    #Test create user does not allow duplicate emails
+    # Test create user does not allow duplicate emails
     def test_create_user_with_duplicate_email(self):
         self.mock_users.insert_one({
             'name': 'Dr. Lehr',
@@ -92,7 +93,34 @@ class Tests_XKX6(unittest.TestCase):
         self.assertEqual(response.status_code, 400)
         self.assertIn('Email already exists', response.get_data(as_text=True))
 
+    # Test a successful user creation
+    def test_create_user_functionality(self): 
+        plain_text_password = "AriosoForTheWin"
 
+        response = self.client.post('/api/create_user', json={
+            "name": "Aria",
+            "email": "aria@arioso.com",
+            "username": "aria",
+            "password": plain_text_password,
+        })
+
+        self.assertEqual(response.status_code, 201)
+        user_in_database = self.mock_users.find_one({"username": "aria"})
+        self.assertIsNotNone(user_in_database)
+
+        # Ensure password in database is hashed
+        self.assertIsInstance(user_in_database["password"], Binary)
+        # Ensure password in database is not plain text password
+        self.assertNotEqual(user_in_database["password"], Binary(plain_text_password.encode('utf-8'))) 
+
+        data = response.get_json()
+        self.assertEqual(data['message'], 'User created successfully')
+        self.assertEqual(data['user']['username'], 'aria')
+        self.assertEqual(data['user']['email'], 'aria@arioso.com')
+        # Ensure password is not in response for security purposes
+        self.assertNotIn('password', data['user']) 
+        
+    #def test_get_user(self):
 
 if __name__ == '__main__':
     unittest.main()
