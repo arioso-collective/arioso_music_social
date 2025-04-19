613bfa3c9590672673eef3112fddc44a4a4830c8
Cameron Salazar <mfq16@txstate.edu>
2025-04-18 22:57:21 -0500

    SCRUM-64 Got a feed to appear on Home page
    Messed with styling for home feed and imported MusicPostFeed to be on HomePage

--- frontend/src/views/HomePage.jsx
+++ frontend/src/views/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './HomePage.module.css';
 import MusicFooter from '../components/FooterBar/MusicFooter';
+import MusicPostFeed from '../components/ProfilePage/MusicPostFeed';
 
 const HomePage = () => {
   return (
     <div className={styles.homeContainer}>
       <h1>Welcome to Arioso</h1>
       <p>Your music community awaits</p>
+      <MusicPostFeed />
       <MusicFooter />
     </div>
   );
