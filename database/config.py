c35d39743b006d416c3bad418bcffda52d222361
Sebastian Zeidler <xkx6@txstate.edu>
2025-04-19 13:12:11 -0500

    SCRUM-135 Set Up Extensions and Database Files
    More functionality from app.py has been split into extensions and database for different responsibilities

--- database/config.py
+++ database/config.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-   SECRET_KEY = os.getenv("JWT_SECRET_KEY", "")
-   JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "")
+   SECRET_KEY = os.getenv("JWT_SECRET_KEY", "your-secret-key")
+   JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "your-secret-key")
    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24) 
