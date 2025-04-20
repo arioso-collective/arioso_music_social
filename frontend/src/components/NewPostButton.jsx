455baa12299243f56cc0c0aeca3a9189065b89bf
Cameron Salazar <mfq16@txstate.edu>
2025-04-20 10:37:35 -0500

    SCRUM-65 Install and changed icon
    Installed a new react package to make the icon for the modal different so it can be more visible

--- frontend/src/components/NewPostButton.jsx
+++ frontend/src/components/NewPostButton.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './NewPostButton.module.css';
+import { FaPen } from "react-icons/fa"; // FontAwesome pen icon
 
 const NewPostButton = ({ onClick }) => {
   return (
     <button className={styles.floatingButton} onClick={onClick}>
-      ➕
+      <FaPen size={24} />
     </button>
   );
 };
