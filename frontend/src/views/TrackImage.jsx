1bf8e74e9dbe8a18337a53b20e30e863eed87245
Juluissa Elias <yqo7@txstate.edu>
2025-04-21 07:23:56 -0500

    SCRUM-123 TrackImage.jsx file
    added file to show track image but as an extension of the search page

---
+++ frontend/src/views/TrackImage.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+//import './TrackImage.css'; // optional for isolated styling
+
+const highlightMatch = (text, query) => {
+  const regex = new RegExp(`(${query})`, 'gi');
+  const parts = text.split(regex);
+  return parts.map((part, i) =>
+    regex.test(part) ? (
+      <span key={i} className="highlight">{part}</span>
+    ) : (
+      <span key={i}>{part}</span>
+    )
+  );
+};
+
+export default function TrackImage({ track, query, searchBy }) {
+  return (
+    <li className="result-item">
+      <img
+        src={track.artworkUrl100}
+        alt={`${track.trackName} album cover`}
+        className="album-art"
+      />
+      <div className="track-info">
+        <div className="track-title">
+          {searchBy === "title"
+            ? highlightMatch(track.trackName, query)
+            : track.trackName}
+        </div>
+        <div className="artist-name">
+          {searchBy === "artist"
+            ? highlightMatch(track.artistName, query)
+            : track.artistName}
+        </div>
+        {track.previewUrl && (
+          <audio controls>
+            <source src={track.previewUrl} type="audio/mpeg" />
+            Your browser does not support the audio element.
+          </audio>
+        )}
+      </div>
+    </li>
+  );
+}
